ee1fca13f662bf3fea65c85ce95d11a03466448a
Hrithik Singh <48889727+hrithik73@users.noreply.github.com>
2021-06-18 00:33:44 +0530

    Time: 64 ms (24.98%), Space: 33.2 MB (32.72%) - LeetHub

--- design-parking-system/design-parking-system.cpp
+++ design-parking-system/design-parking-system.cpp
@@ -18,7 +18,6 @@ public:
 };
 
 /**
-    Submitted By : Hrithik Singh
  * Your ParkingSystem object will be instantiated and called as such:
  * ParkingSystem* obj = new ParkingSystem(big, medium, small);
  * bool param_1 = obj->addCar(carType);
