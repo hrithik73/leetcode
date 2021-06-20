315325698250cdc2ca93bab492527b93c7ffb56c
Hrithik Singh <48889727+hrithik73@users.noreply.github.com>
2021-06-20 12:22:00 +0530

    Time: 40 ms (55.49%), Space: 8.3 MB (18.60%) - LeetHub

---
+++ count-good-triplets/count-good-triplets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+       int len=arr.size(),res=0;
+        
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                for(int k=j+1;k<len;k++){
+                    if(abs(arr[i]-arr[j])<=a && abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c)
+                        res++;
+                }
+            }
+        }
+        
+        return res;
+        
+    }
+};
