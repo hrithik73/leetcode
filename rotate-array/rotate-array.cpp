0802d44c9986138b843dec649111e69a070552ce
Hrithik Singh <48889727+hrithik73@users.noreply.github.com>
2021-06-24 15:53:46 +0530

    Time: 28 ms (69.81%), Space: 24.9 MB (71.42%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
   
-    void reverseArray(vector<int>&nums,int start,int end){
-        int temp=0;
-        while(start<end){
-            temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
-            start++;
-            end--;
-        }
-    }
+//     void reverseArray(vector<int>&nums,int start,int end){
+//         int temp=0;
+//         while(start<end){
+//             temp=nums[start];
+//             nums[start]=nums[end];
+//             nums[end]=temp;
+//             start++;
+//             end--;
+//         }
+//     }
     
     
     void rotate(vector<int>& nums, int k) {
-        if(k==0)
-            return;
-        if(k>nums.size())
-            k=k%nums.size();
+//         if(k==0)
+//             return;
+//         if(k>nums.size())
+//             k=k%nums.size();
             
-        reverseArray(nums,0,nums.size()-1);
-        reverseArray(nums,0,k-1);
-        reverseArray(nums,k,nums.size()-1);
+//         reverseArray(nums,0,nums.size()-1);
+//         reverseArray(nums,0,k-1);
+//         reverseArray(nums,k,nums.size()-1);
+        
+         k %=nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
         
     }
     
