70bf3db95a447c080d2d2c34407c8d7b267b8573
Hrithik Singh <48889727+hrithik73@users.noreply.github.com>
2021-06-26 11:30:22 +0530

    Time: 4 ms (96.66%), Space: 7 MB (38.39%) - LeetHub

---
+++ isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,int> hash1;
+        unordered_map<char,int> hash2;
+        
+        if(s.size()!=t.size())
+            return false;
+        
+        for(int i=0;i<s.size();i++){
+            if((hash1.find(s[i])==hash1.end()) && (hash2.find(t[i])==hash2.end()))
+            {
+                hash1[s[i]]=i+1;
+                hash2[t[i]]=i+1;
+            }
+            
+            else if((hash1.find(s[i])!=hash1.end()) && (hash2.find(t[i])!=hash2.end())){
+                if(hash1[s[i]]!=hash2[t[i]])
+                    return false;   
+            }
+            else
+                return false;
+            //Me:-Gadha Prasad
+        }
+        
+        return true;        
+    
+    }
+};
