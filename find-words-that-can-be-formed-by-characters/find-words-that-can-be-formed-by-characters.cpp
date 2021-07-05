67d34b0fc27d71428992f7ae0cd7bf18eb998240
Hrithik Singh <48889727+hrithik73@users.noreply.github.com>
2021-07-05 11:25:55 +0530

    Time: 124 ms (36.73%), Space: 23.7 MB (39.50%) - LeetHub

---
+++ find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        unordered_map <char,int> hash;
+        unordered_map <char,int> temp;
+        bool flag=false;
+        int count=0;
+        
+        for(int i=0;i<chars.size();i++)
+        {
+          hash[chars[i]]++;
+        }
+    
+        for(int i=0;i<words.size();i++){
+             temp=hash;
+             flag=false;
+             string word=words[i];
+             for(int j=0;j<word.size();j++){
+                   if(temp[word[j]]){
+                       temp[word[j]]--;
+                   }
+                else
+                    flag=true;
+             }
+            if(!flag)
+                count+=word.size();
+                
+        }
+        return count;
+        
+    }
+};
