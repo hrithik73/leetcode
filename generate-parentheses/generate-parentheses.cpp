3f5f6ab6b2e0eb6618b34f512dab3db697639769
Hrithik Singh <48889727+hrithik73@users.noreply.github.com>
2021-07-10 15:36:29 +0530

    Time: 12 ms (14.49%), Space: 13.7 MB (59.95%) - LeetHub

--- generate-parentheses/generate-parentheses.cpp
+++ generate-parentheses/generate-parentheses.cpp
@@ -1,44 +1,73 @@
 class Solution {
 public:
-    
-    bool isValid(string str ){
-        stack<char> st;
-        
-        for(int i=0;i<str.size();i++){
-            if(str[i]=='(')
-                st.push('(');
-            else if(st.empty())
-                return false;
-            else
-                st.pop();
-        }
-        if(!st.empty())
-            return false;
-
-        return true;    
-    }
-        
-    void helper(vector<string> &res,string currentStr,int n){
-        
-        if(currentStr.size()==2*n){
-            if(isValid(currentStr)){
-                res.push_back(currentStr);
-            }    return ;
-        }
-        
-        helper(res,currentStr+'(',n);
-        helper(res,currentStr+')',n);
-        
-    }
-    
-    
     vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        string currStr="";
-        helper(res,currStr,n);
-            
-        return res;
+        vector<string> ans;
+        string out= "";
+        solve(ans, out, n, n);
+        return ans;
+
     }
 
-
+    void solve(vector<string> &ans ,string out, int left , int right){
+        if(left==0 &&right==0){
+            ans.push_back(out);
+            return;
+        }
+        
+         if(left>0) solve(ans,out+'(',left-1,right);
+         if(right>left) solve(ans,out+')',left,right-1);
+        
+        
+            
+    }
+    
 };
+
+
+// BruteForce!
+// class Solution {
+// public:
+
+//     bool isValid(string str ){
+//         stack<char> st;
+
+//         for(int i=0;i<str.size();i++){
+//             if(str[i]=='(')
+//                 st.push('(');
+//             else if(st.empty())
+//                 return false;
+//             else
+//                 st.pop();
+//         }
+//         if(!st.empty())
+//             return false;
+
+//         return true;    
+//     }
+
+//     void helper(vector<string> &res,string currentStr,int n){
+
+//         if(currentStr.size()==2*n){
+//             if(isValid(currentStr)){
+//                 res.push_back(currentStr);
+//             }    return ;
+//         }
+
+//         helper(res,currentStr+'(',n);
+//         helper(res,currentStr+')',n);
+
+//     }
+
+
+//     vector<string> generateParenthesis(int n) {
+//         vector<string> res;
+//         string currStr="";
+//         helper(res,currStr,n);
+
+//         return res;
+//     }
+
+
+// };
+
+
